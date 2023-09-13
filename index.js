b0d9672a3bf096718c83e6638602d85fdf70fc0d
Sammy Gonzalez III <sammygonzo951@gmail.com>
2023-09-12 19:55:02 -0700

    Setup getting currencies and creating labels to pass to index.ejs

--- index.js
+++ index.js
@@ -4,17 +4,32 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+const API_URL = 'https://www.frankfurter.app';
 
 app.use(express.static('public'));
 
 app.get('/', async (req, res) => {
     try {
-        res.render('index.ejs');
+        const response = await axios.get(API_URL + '/currencies');
+        const currencies = getCurrencyLabels(response.data);
+        const data = {
+            currencies: currencies,
+        }
+        res.render('index.ejs', data);
     } catch {
         
     }
-})
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
+function getCurrencyLabels(response){
+    let currencies = [];
+    for (let key in response){
+        currencies.push(`${response[key]} (${key})`);
+    }
+    console.log(currencies);
+    return currencies;
+};
