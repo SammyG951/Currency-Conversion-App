b7eccd2e3d1662f229ccb29e4da178cebe69d0df
Sammy Gonzalez III <sammygonzo951@gmail.com>
2023-09-12 21:08:27 -0700

    Setup ejs form and optimized passing data to index.ejs

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ const port = 3000;
 const API_URL = 'https://www.frankfurter.app';
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', async (req, res) => {
     try {
         const response = await axios.get(API_URL + '/currencies');
-        const currencies = getCurrencyLabels(response.data);
         const data = {
-            currencies: currencies,
+            currencies: response.data,
         }
         res.render('index.ejs', data);
     } catch {
@@ -21,15 +21,11 @@ app.get('/', async (req, res) => {
     }
 });
 
+app.post('/convert-currency', async(req, res) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
-
-function getCurrencyLabels(response){
-    let currencies = [];
-    for (let key in response){
-        currencies.push(`${response[key]} (${key})`);
-    }
-    console.log(currencies);
-    return currencies;
-};
